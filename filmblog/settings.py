12917c7e76b88165f95987b2af6e2ba843b35105
Christo107 <christopher.odwyer82@gmail.com>
2023-06-11 12:51:55 +0000

    Add admin panel functionality

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-import os
+
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
