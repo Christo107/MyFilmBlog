560d97fe239de52955a805a074ca2103f193bd1e
Christo107 <christopher.odwyer82@gmail.com>
2023-06-11 11:42:24 +0000

    Add env.py file

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'filmblog.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-    
+
 DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
  }
