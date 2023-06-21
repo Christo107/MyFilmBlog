c09aa178955858afe9f6b4f512fe43ff87096747
Christo107 <christopher.odwyer82@gmail.com>
2023-06-21 10:43:55 +0000

    Fix actor detail url path

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('home/', views.ActorList.as_view(), name='actor_list'),
-    path('/', views.ActorDetail.as_view(), name='actor_detail'),
+    path('', views.ActorDetail.as_view(), name='actor_detail'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
