de94303b50626a9584c70c46bceb7696aeb71dad
Christo107 <christopher.odwyer82@gmail.com>
2023-07-31 17:09:41 +0000

    Update auth password validators

--- filmblog/settings.py
+++ filmblog/settings.py
@@ -133,9 +133,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.\
                 MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 9,
-        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.\
